9d7bee5ec55ebbf83391481115796f645136aa81
KaroAnde <ksa@onics.com>
2025-06-20 13:29:49 +0200

    Add new xcode project

---
+++ AdVendtureTime/AdVendtureTime/AdVendtureTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdVendtureTimeApp.swift
+//  AdVendtureTime
+//
+//  Created by Karoline Skumsrud Andersen on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AdVendtureTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdVendtureTime/AdVendtureTimeUITests/AdVendtureTimeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AdVendtureTimeUITestsLaunchTests.swift
+//  AdVendtureTimeUITests
+//
+//  Created by Karoline Skumsrud Andersen on 20/06/2025.
+//
+
+import XCTest
+
+final class AdVendtureTimeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
